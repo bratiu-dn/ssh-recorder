81e775306d0fb3c97c6286a93d4f5858f45481df
Bogdan Ratiu <bratiu-dn@drivenets.com>
2024-02-17 20:46:30 +0100

    Final touches on the show command

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 APP = ['dn_recorder_ui.py']
 DATA_FILES = [
-    ('', ['DN-Logo.png', 'script.txt'])
+    ('', ['DN-Logo.png', 'script.txt', 'expect.exp'])
 ]
 OPTIONS = {
     'argv_emulation': False,
-    'packages': ['jira', 'PyQt5', 'qtawesome'],
+    'packages': ['jira', 'PyQt5', 'qtawesome', 'paramiko', 'cffi'],
     'includes': ['charset_normalizer', 'chardet'],
     'iconfile': 'SSHRecorder.icns'  # path to your .icns file
 
